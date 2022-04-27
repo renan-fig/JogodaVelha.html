d140735c5b27cfc6a005db1ba5df1ed09a4a23d9
Renan Figueiredo <renan.fig.dasilva@gmail.com>
2022-04-27 13:15:47 -0300

    jogo da velha web

---
+++ velhagame.js
@@ -0,0 +1,118 @@
+function escolha(id){
+    var quadrado = document.getElementById(id);
+
+    if(vencedor !== null){
+        return;
+    }
+    if(quadrado.innerHTML !== '-'){
+        return;
+    }
+
+    quadrado.innerHTML = jogador;
+    quadrado.style.color = '#000';
+    
+    if(jogador === 'X'){
+        jogador = 'O';
+    }else{
+        jogador = 'X';
+    }
+    mudarJogador(jogador);
+    checaVencedor(vencedor);
+}
+
+function mudarJogador(valor){
+    jogador = valor;
+    jogadorSelecionado.innerHTML = jogador;
+}
+
+function checaVencedor(){
+    var quadrado1 = document.getElementById('1')
+    var quadrado2 = document.getElementById('2')
+    var quadrado3 = document.getElementById('3')
+    var quadrado4 = document.getElementById('4')
+    var quadrado5 = document.getElementById('5')
+    var quadrado6 = document.getElementById('6')
+    var quadrado7 = document.getElementById('7')
+    var quadrado8 = document.getElementById('8')
+    var quadrado9 = document.getElementById('9')
+
+    if(checagem(quadrado1, quadrado2, quadrado3)){
+        mudaCor(quadrado1, quadrado2, quadrado3);
+        mudarVencedor(quadrado1);
+        return;
+    }
+    if(checagem(quadrado4, quadrado5, quadrado6)){
+        mudaCor(quadrado4, quadrado5, quadrado6);
+        mudarVencedor(quadrado4);
+        return;
+    }
+    if(checagem(quadrado7, quadrado8, quadrado9)){
+        mudaCor(quadrado7, quadrado8, quadrado9);
+        mudarVencedor(quadrado7);
+        return;
+    }
+    if(checagem(quadrado1, quadrado4, quadrado7)){
+        mudaCor(quadrado1, quadrado4, quadrado7);
+        mudarVencedor(quadrado1);
+        return;
+    }
+    if(checagem(quadrado2, quadrado5, quadrado8)){
+        mudaCor(quadrado2, quadrado5, quadrado8);
+        mudarVencedor(quadrado2);
+    }
+    if(checagem(quadrado3, quadrado6, quadrado9)){
+        mudaCor(quadrado3, quadrado6, quadrado9);
+        mudarVencedor(quadrado3);
+        return;
+    }
+    if(checagem(quadrado1, quadrado5, quadrado9)){
+        mudaCor(quadrado1, quadrado5, quadrado9);
+        mudarVencedor(quadrado1);
+        return;
+    }
+    if(checagem(quadrado3, quadrado5, quadrado7)){
+        mudaCor(quadrado3, quadrado5, quadrado7);
+        mudarVencedor(quadrado3);
+        return;
+    }
+
+}
+function checagem(quad1, quad2, quad3){
+    var igual = false;
+
+    if(quad1.innerHTML !== '-' && quad1.innerHTML === quad2.innerHTML && quad2.innerHTML === quad3.innerHTML){
+        igual = true;
+    }
+
+    return igual;
+}
+function mudaCor(quad1, quad2, quad3){
+    quad1.style.background = '#0f0';
+    quad2.style.background = '#0f0';
+    quad3.style.background = '#0f0';
+}
+function mudarVencedor(venc){
+    vencedor = venc.innerHTML;
+    vencedorSelecionado.innerHTML = vencedor;
+}
+
+function reiniciar(){
+    vencedor = null;
+    vencedorSelecionado.innerHTML = '';
+
+    for (var i = 1; i <= 9; i++) {
+        var quadrado = document.getElementById(i);
+        quadrado.style.background = 'rgb(210, 217, 217)';
+        quadrado.style.color = 'rgb(210, 217, 217)';
+        quadrado.innerHTML = '-';
+    }
+
+    mudarJogador('X');
+}
+let quadrados = document.getElementsByClassName('quadrado');
+let jogador, vencedor = null;
+let jogadorSelecionado = document.getElementById(elementId = "player-sel");
+mudarJogador('X');
+let vencedorSelecionado = document.getElementById(elementId = "player-win");
+mudarJogador('X');
+
